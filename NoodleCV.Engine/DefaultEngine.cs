7d29c4760d70cf2e4e3e03e459c98566b3e20878
BartShoot <dobija.bartosz@gmail.com>
2024-03-03 18:23:21 +0100

    fleshing out engine interface

--- NoodleCV.Engine/DefaultEngine.cs
+++ NoodleCV.Engine/DefaultEngine.cs
@@ -6,9 +6,28 @@ public class DefaultEngine : IEngine
 {
     private List<IOperation> _operations = new();
 
+    public void AddNode(IOperation operation)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveNode(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateNode(IOperation operation)
+    {
+        throw new NotImplementedException();
+    }
 
     public Mat GetImage(int id)
     {
         return _operations.Find(operation => operation.Id == id)?.GetImage() ?? throw new ImageNotFoundException();
+    }
+
+    private void RecalculateChildNodes(IOperation changedOperation)
+    {
+        throw new NotImplementedException();
     }
 }
