6dde80d7695ca938673efadb5aaed73cd8cac634
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2024-03-11 22:54:19 +0100

    Move parameters outside of main window

---
+++ NoodleCV.App/Views/ParameterView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NoodleCV.App.Views;
+
+public partial class ParameterView : UserControl
+{
+    public ParameterView()
+    {
+        InitializeComponent();
+    }
+}
