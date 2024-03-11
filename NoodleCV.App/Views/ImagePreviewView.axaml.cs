55c762523a0a37cc96813f03610d57e9adca973b
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2024-03-11 22:55:50 +0100

    Move image preview outside of MainWindow

---
+++ NoodleCV.App/Views/ImagePreviewView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NoodleCV.App.Views;
+
+public partial class ImagePreviewView : UserControl
+{
+    public ImagePreviewView()
+    {
+        InitializeComponent();
+    }
+}
