9c0af983fa9bc0ade1619b90e8dc1d7ec53668b6
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2024-03-31 18:01:05 +0200

    blocking

--- NoodleCV.App/ViewModels/ParameterViewModel.cs
+++ NoodleCV.App/ViewModels/ParameterViewModel.cs
@@ -1,10 +1,12 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using NoodleCV.App.CustomControls.ViewModel;
+using NoodleCV.App.Operations;
 
 namespace NoodleCV.App.ViewModels;
 
 public class ParameterViewModel : ObservableObject
 {
+    public BlurViewModel BlurViewModel { get; } = new();
     public CustomSliderViewModel CustomSliderViewModel { get; } = new(5, 100, "My Custom Slider");
 
     public CustomFilePickerViewModel CustomFilePickerViewModel { get; } = new("My Custom File Picker");
